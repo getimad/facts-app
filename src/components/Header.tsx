ebc9a1931991c46248ff43307ee040c200fd7b5f
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-14 00:42:47 +0100

    feat: Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import whiteLogo from "../assets/logos/logo-white.svg";
+
+function Header() {
+  return (
+    <header
+      className="
+        flex
+        justify-between
+      ">
+      <img
+        src={whiteLogo}
+        className="
+          h-8
+          w-8 
+        " />
+      <button
+        className="
+          px-5
+          border-white
+          border-2
+          rounded-full
+          duration-300
+          capitalize
+          hover:border-green-400
+          hover:text-green-400
+        ">
+        share a fact
+      </button>
+    </header>
+  );
+}
+
+export default Header;
