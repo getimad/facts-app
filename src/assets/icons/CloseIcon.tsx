792823eaf579e75f8758f5ea6713bd6dee98de85
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-14 22:28:44 +0100

    feat: Add close button to FactShare component

---
+++ src/assets/icons/CloseIcon.tsx
@@ -0,0 +1,9 @@
+function CloseIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+    </svg>
+  );
+}
+
+export default CloseIcon;
