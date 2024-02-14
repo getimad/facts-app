dc86fb627dfb57c524c92a072016be29bb2fe228
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-14 16:28:07 +0100

    Add FactContainer to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import Header from "./components/Header";
+import FactContainer from "./components/FactContainer";
 
 function App() {
   return (
-    <div className="p-5 mx-auto max-w-6xl">
+    <div className="
+      p-5
+      mx-auto
+      max-w-6xl">
       <Header />
+      <div className="
+        flex font-inter">
+        <aside className="
+          w-80">
+          Something
+        </aside>
+        <main className="h-full flex-grow">
+          <FactContainer />
+        </main>
+      </div>
     </div>
   );
 }
