c62af18a73a458d37a1b8369ed880f36a372556b
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-14 13:23:45 +0100

    Add UpIcon and DownIcon icons

---
+++ src/assets/icons/UpIcon.tsx
@@ -0,0 +1,9 @@
+function UpIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+    </svg>
+  );
+}
+
+export default UpIcon;
