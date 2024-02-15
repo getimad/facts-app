5a627538c625ba98d2a14d575e762a7970261102
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-15 12:01:47 +0100

    Add another fact to default facts in store

--- src/store.ts
+++ src/store.ts
@@ -9,14 +9,43 @@ interface FactStore {
 }
 
 const useFactStore = create<FactStore>((set) => ({
-  facts: [{id: 1, content:"dsjlfs dljqs", voteCount: 0, source:"#", category:"something"}],
-  setFact: (fact: Fact) => set((state) => ({facts: [...state.facts, fact]})),
-  upVote: (id: number) => set((state) => ({
-    facts: state.facts.map(f => (f.id === id) ? {...f, voteCount: f.voteCount + 1} : f)
-  })),
-  downVote: (id: number) => set((state) => ({
-    facts: state.facts.map(f => (f.id === id) ? {...f, voteCount: f.voteCount - 1} : f)
-  }))
+  facts: [
+    {
+      id: 1,
+      content: "Web Development is Only About Coding.",
+      voteCount: -2,
+      source: "#",
+      category: "techonology",
+    },
+    {
+      id: 2,
+      content: "Web Development is Easy.",
+      voteCount: -19,
+      source: "#",
+      category: "techonology",
+    },
+    {
+      id: 3,
+      content:
+        "Web development is a field that is constantly evolving. New technologies, frameworks, and tools are introduced regularly, and developers need to keep learning to stay up-to-date.",
+      voteCount: 57,
+      source: "#",
+      category: "techonology",
+    },
+  ],
+  setFact: (fact: Fact) => set((state) => ({ facts: [...state.facts, fact] })),
+  upVote: (id: number) =>
+    set((state) => ({
+      facts: state.facts.map((f) =>
+        f.id === id ? { ...f, voteCount: f.voteCount + 1 } : f
+      ),
+    })),
+  downVote: (id: number) =>
+    set((state) => ({
+      facts: state.facts.map((f) =>
+        f.id === id ? { ...f, voteCount: f.voteCount - 1 } : f
+      ),
+    })),
 }));
 
 export default useFactStore;
