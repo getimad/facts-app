cd3a9dd5cbadc21af642daf04a2dbe30b2f91bf2
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-15 12:03:24 +0100

    Add titles for Categories and Facts in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,12 @@ function App() {
       <Header setIsFactShareOpen={(isOpen: boolean) => setIsFactShareOpen(isOpen)} />
       <div className="
         flex font-inter">
-        <aside className="
-          w-80">
+        <aside className="w-80">
+          <h3 className="text-2xl font-bold mb-5">Categories</h3>
           Something
         </aside>
         <main className="h-full flex-grow">
+          <h3 className="text-4xl font-bold mb-5">Facts</h3>
           <FactContainer />
         </main>
         <div className={`fixed h-screen w-screen top-0 left-0 transition-transform duration-150 ease-in-out transform ${isFactShareOpen ? 'scale-100' : 'scale-0'}`}>
