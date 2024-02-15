1538414e0c9753b475bd90dc643f5bd6114c5f67
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-15 13:04:19 +0100

    style: Move share a fact button from Header to App and refactor style

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,29 @@ import Header from "./components/Header";
 import FactContainer from "./components/FactContainer";
 import FactShare from "./components/FactShare";
 import { useState } from "react";
+import ShareIcon from "./assets/icons/ShareIcon";
 
 function App() {
   const [isFactShareOpen, setIsFactShareOpen] = useState(false);
 
   return (
-    <div className="
-      p-5
-      mx-auto
-      max-w-6xl">
-      <Header setIsFactShareOpen={(isOpen: boolean) => setIsFactShareOpen(isOpen)} />
-      <div className="
-        flex font-inter">
+    <div className="p-5 mx-auto max-w-6xl">
+      <Header />
+      <div className="flex font-inter">
         <aside className="w-80">
           <h3 className="text-2xl font-bold mb-5">Categories</h3>
           Something
         </aside>
         <main className="h-full flex-grow">
-          <h3 className="text-4xl font-bold mb-5">Facts</h3>
+          <div className="flex justify-between items-center mb-7">
+            <h3 className="text-4xl font-bold">Facts</h3>
+            <button onClick={() => setIsFactShareOpen(true)}
+              className="bg-green-950 px-5 py-2 rounded-full duration-100 hover:text-green-400 flex gap-4 items-center capitalize"
+            >
+              Share a fact
+              <ShareIcon />
+            </button>
+          </div>
           <FactContainer />
         </main>
         <div className={`fixed h-screen w-screen top-0 left-0 transition-transform duration-150 ease-in-out transform ${isFactShareOpen ? 'scale-100' : 'scale-0'}`}>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,12 @@
 import whiteLogo from "../assets/logos/logo-white.svg";
 
-interface Props {
-  setIsFactShareOpen: (isOpen: boolean) => void;
-}
-
-function Header({ setIsFactShareOpen }: Props) {
+function Header() {
   return (
-    <header
-      className="
-        flex
-        justify-between
-        font-pr
-        mb-12
-      ">
+    <header className="flex justify-between mb-12 font-semibold text-sm">
       <img
         src={whiteLogo}
-        className="
-          h-8
-          w-8 
-        " />
-      <button
-        className="
-          px-5
-          border-white
-          border-2
-          rounded-full
-          duration-300
-          capitalize
-          hover:border-green-400
-          hover:text-green-400"
-        onClick={() => setIsFactShareOpen(true)}>
-        share a fact
-      </button>
+        className="h-8 w-8"
+      />
     </header>
   );
 }
