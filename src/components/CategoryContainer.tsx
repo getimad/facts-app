9774640f2c2face7bb20e34add6591db6dbe2ea5
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-15 21:13:07 +0100

    feat: Add all category button for categories

--- src/components/CategoryContainer.tsx
+++ src/components/CategoryContainer.tsx
@@ -3,6 +3,7 @@ import CategoreyBtn from "./CategoryBtn";
 
 function CategoreyContainer() {
   const categories: Category[] = [
+    { title: "all", bgColor: "#0D0D0D", borderColor: "#1F1F1F" },
     { title: "technology", bgColor: "#000531", borderColor: "#001063" },
     { title: "sience", bgColor: "#002729", borderColor: "#005F5F" },
     { title: "health", bgColor: "#002F05", borderColor: "#005703" },
