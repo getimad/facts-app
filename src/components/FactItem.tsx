70df8081bf6b8b1b776f7a97237448d61fc0e9ba
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-02-15 12:00:44 +0100

    style: Refactor style of FactItem component

--- src/components/FactItem.tsx
+++ src/components/FactItem.tsx
@@ -7,30 +7,16 @@ interface Props {
 
 function FactItem({ fact }: Props) {
   return (
-    <li
-      className="p-5
-      border
-      border-green-900
-      rounded-2xl
-      bg-green-950
-      flex
-      justify-between
-      mt-3"
-    >
-      <p
-        className="
-        w-96">
+    <li className="p-5 border border-green-900 rounded-2xl bg-green-950 flex justify-between items-center mt-3">
+      <p className="w-[30rem]">
         {fact.content}
-        <a
-          href={fact.source}
-          className="
-          font-bold
-          text-green-800
-          pl-2">
+        <a href={fact.source} className="font-bold text-green-800 pl-2">
           (Source)
         </a>
       </p>
-      <span className="flex items-center text-sm bg-green-900 px-3 m-4 rounded-full">{fact.category}</span>
+      <span className="flex items-center text-sm bg-green-900 px-5 h-10 rounded-full">
+        {fact.category}
+      </span>
       <Vote id={fact.id} />
     </li>
   );
